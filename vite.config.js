d28553d89fc7dbd7d3f842ea4d21f3879e8df018
Xilarie1 <martin8.kodehode@gmail.com>
2025-09-15 11:16:27 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Xilarie1/Restaurant",
+  base: "Restaurant",
 });
